52d7c1ab8df948a668ff63fb479569ea9e385254
Aaheer17 <farzanaahmad.17@gmail.com>
2025-04-30 16:29:56 -0400

    Update config_creation.py

--- config_creation.py
+++ config_creation.py
@@ -3,35 +3,32 @@ import itertools
 import torch
 import json
 import os
+#current file will generate 91 configurations..
+#commented out params are for transformer
 param_grid = {
     'block_out_channels': [(4, 8, 16, 16), (4, 4, 8, 8)],
     'cross_attention_dim': [8, 16],
-    'trans_dim': [32],
+    #'trans_dim': [32],
     'layers_per_block': [2],
     'norm_num_groups': [4],
-    'heads': [2],
-    'num_encoder_layers': [2],
-    'num_decoder_layers': [2],
-    'dropout_p': [0.05, 0.1],
-    'diffusion_timestep': [100],
-    'schedule_type': ['cosine']
+    #'heads': [2],
+    #'num_encoder_layers': [2],
+    #'num_decoder_layers': [2],
+    #'dropout_p': [0.05, 0.1],
+    'diffusion_timestep': [100, 200],
+    'schedule_type': ['DDPM','DDIM'],
+    'num_inference_steps':[50,100],  # should be less than diffusion_step
+    'prediction_type':['sample','epsilon','v-prediction']
+    
 }
 # Generate all possible combinations
 keys, values = zip(*param_grid.items())
 param_combinations = [dict(zip(keys, v)) for v in itertools.product(*values)]
-# fixed_params = {
-#     'max_seq_len': 45,
-#     'device': torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# }
-
-# # Merge fixed params into each combo
-# for combo in param_combinations:
-#     combo.update(fixed_params)
-    
 
 
+#DD stands for denoising diffusion
 for idx, params in enumerate(param_combinations):
-    config_dir = './configs_cosine'
+    config_dir = './configs_DD'
     os.makedirs(config_dir, exist_ok=True)
     config_path = os.path.join(config_dir, f'config_{idx}.json')
 
